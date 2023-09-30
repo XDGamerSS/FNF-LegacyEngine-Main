f538e7518a9baf6b005ca1dcd6d1272bb16d3a91
XDGamerSS <146037729+XDGamerSS@users.noreply.github.com>
2023-09-30 15:41:33 -0300

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -2313,7 +2313,7 @@ class PlayState extends MusicBeatState
 		if(ratingName == '?') {
 			scoreTxt.text = 'Score: ' + songScore + ' | Combo Breaks: ' + songMisses + ' | Accuracy: ' + ratingName;
 		} else {
-			scoreTxt.text = 'Score: ' + songScore + ' | Combo Breaks: ' + songMisses + ' | Accuracy: ' + Highscore.floorDecimal(ratingPercent * 100, 2) + '%)' + ' [ ' + ratingFC + ' ] ';
+			scoreTxt.text = 'Score: ' + songScore + ' | Combo Breaks: ' + songMisses + ' | Accuracy: ' + Highscore.floorDecimal(ratingPercent * 100, 2) + '%' + ' [ ' + ratingFC + ' ] ';
 		}
 
 		if(botplayTxt.visible) {
